42f0f8a49b3b6cc1fd9adbcaf46f07f98f373fac
HariniRK <harinirk17@gmail.com>
2022-09-19 17:50:36 +0530

    Craeting nav bar, angular template, services injecting into components, using conditionals, bootstrap, observables, aync pipe, adding homepage, parent to child communication and c to p

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -15,7 +15,5 @@ namespace API.Data
        }
        public DbSet<AppUser> Users { get; set; }
        
-       
-        
     }
 }
---
+++ client/src/app/_models/user.ts
@@ -0,0 +1,5 @@
+ //interface in c# and ts are little bit different. In ts, we can use it to specify that something is a type of something
+ export interface User{
+    username : string;
+    token : string;
+ }
