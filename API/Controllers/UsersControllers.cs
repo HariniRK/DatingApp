34cf5cdd31b9fad739cf51829965eec140f646b2
HariniRK <harinirk17@gmail.com>
2022-09-30 18:38:47 +0530

    Adding datetime, entity framework relationships, generating and using seed data, Creating Repository, adding DTO for members, Using and Configuring Automapper queryable extensions

--- API/Controllers/UsersControllers.cs
+++ API/Controllers/UsersControllers.cs
@@ -7,36 +7,39 @@ using API.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Authorization;
+using API.Interfaces;
+using API.DTOs;
+using AutoMapper;
 
 namespace API.Controllers
 {
-   
+    [Authorize]
     public class UsersControllers : BaseApiController
     {
-        private readonly DataContext _context;
+        private readonly IUserRepository _userRepository;
+        private readonly IMapper _mapper;
 
-        public UsersControllers(DataContext context)
+        public UsersControllers(IUserRepository userRepository, IMapper mapper)
         {
-            _context = context;
+            _mapper = mapper;
+            _userRepository = userRepository;
+            
 
         }
         //api/users
         [HttpGet]
-        [AllowAnonymous]
-        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
         {
-            return await _context.Users.ToListAsync();
-
+            var users = await _userRepository.GetMembersAsync();
+            return Ok(users);
         }
 
-        [Authorize]
-        // api/user/2or3
-        [HttpGet("{id}")]
-        public async Task<ActionResult<AppUser>> GetUser(int id)
-        {
-            return await _context.Users.FindAsync(id);
 
+        // api/user/2or3
+        [HttpGet("{username}")]
+        public async Task<ActionResult<MemberDto>> GetUser(string username)
+        {
+            return await _userRepository.GetMemberAsync(username);
         }
     }
-
 }
---
+++ API/Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.DTOs;
+using API.Entities;
+using API.Extensions;
+using AutoMapper;
+
+namespace API.Helpers
+{
+    //The idea of automapper is it helps us from one object to another.
+
+    public class AutoMapperProfiles : Profile
+    {
+        protected AutoMapperProfiles()
+        {
+            CreateMap<AppUser, MemberDto>()
+            .ForMember(dest => dest.PhotoUrl, opt => opt.MapFrom(src => 
+            src.Photos.FirstOrDefault(x => x.IsMain).Url))
+            .ForMember(dest => dest.Age, opt => opt.MapFrom(src => src.DateOfBirth.CalculateAge()));
+            CreateMap<Photo, PhotoDto>();
+        }
+    }
+}
