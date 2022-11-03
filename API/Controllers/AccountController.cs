612927895f81345447c9b2db82cc76ceb54bb4b1
HariniRK <harinirk17@gmail.com>
2022-11-03 22:42:42 +0530

    configuring clodinary account, adding photo service, updating and testing photo upload, updating created atroute method, adding photo editor component, photo uploader, set main photo and deletephoto api and client, adding main photo in the nav bar

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -50,7 +50,9 @@ namespace API.Controllers
         [HttpPost("login")]
         public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
         {
-            var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
+            var user = await _context.Users
+            .Include(p => p.Photos)
+            .SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
 
             if(user == null) return Unauthorized("Invalid username");
 
@@ -62,11 +64,11 @@ namespace API.Controllers
             {
                 if(computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid password");
             }
-
             return new UserDto
             {
                 Username =  user.UserName,
-                Token = _tokenService.CreateToken(user)
+                Token = _tokenService.CreateToken(user),
+                PhotoUrl = user.Photos.FirstOrDefault(x =>x.IsMain)?.Url
             };
         }
         private async Task<bool> UserExists(string username)
--- API/DTOs/UserDto.cs
+++ API/DTOs/UserDto.cs
@@ -10,6 +10,10 @@ namespace API.DTOs
         public string Username { get; set; }
         
         public string Token { get; set; }
+
+        public string PhotoUrl { get; set; }
+        
+        
        
     }
 }
